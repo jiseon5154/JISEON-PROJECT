035d2b10402635d65085ea0f827f85714e3c43a9
DDJ-Seo <dongjoo0714@gmail.com>
2021-08-03 14:27:13 +0900

    section-18 까지 이미지 삽입 완료

--- docs/common.js
+++ docs/common.js
@@ -8,3 +8,14 @@ gsap.from('.img-1', {
     start:'0 75%'
   }
 });
+
+gsap.from('.img-2', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:300,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-2',
+    start:'0 75%'
+  }
+});
