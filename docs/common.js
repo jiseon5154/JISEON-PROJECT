a7a1159ea7cd65bc69a64d5bd412fa17dfd686a4
DDJ-Seo <dongjoo0714@gmail.com>
2021-08-03 16:25:24 +0900

    최종 작업 완료

--- docs/common.js
+++ docs/common.js
@@ -5,7 +5,7 @@ gsap.from('.img-1', {
   duration:1.2,
   scrollTrigger:{
     trigger:'.img-1',
-    start:'0 75%'
+    start:'0 85%'
   }
 });
 
@@ -16,6 +16,105 @@ gsap.from('.img-2', {
   duration:1.2,
   scrollTrigger:{
     trigger:'.img-2',
-    start:'0 75%'
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-3', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-4', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-5', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-5',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-6', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-5',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-9', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:0.7,
+  scrollTrigger:{
+    trigger:'.img-9',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-10', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.2,
+  scrollTrigger:{
+    trigger:'.img-9',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-11', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:1.8,
+  scrollTrigger:{
+    trigger:'.img-9',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-12', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:2.5,
+  scrollTrigger:{
+    trigger:'.img-9',
+    start:'0 85%'
+  }
+});
+
+gsap.from('.img-13', {
+  ease: Expo.easeIn,
+  opacity:0,
+  x:0,
+  duration:2.5,
+  scrollTrigger:{
+    trigger:'.img-9',
+    start:'0 85%'
   }
 });
